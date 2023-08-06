9a719ae0c2b1735db12f03c4a8e0c13ffd99af0f
singhnavin19 <singhnavin19290@gmail.com>
2023-08-06 11:52:08 +0530

    basic variable

---
+++ variable/VariableBasic.py
@@ -0,0 +1,24 @@
+#dataType varName=value   static Type language
+
+by=10  #dynamic Type language
+print(by)
+print(type(by))
+
+#///function (arguments)
+
+_name1="Hiranmoy"
+print(_name1)
+
+#case-sensitive
+a=10;
+#print(A) //got error due to case sensitive
+
+
+
+#multiple assignement
+
+v=10
+print(v)
+v="navin"
+print(v)
+
