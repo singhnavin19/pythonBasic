534b45e7903284d5b093de171568a248cac45794
singhnavin19 <singhnavin19290@gmail.com>
2023-08-06 12:04:57 +0530

    data Type

---
+++ datatype/DataTypeBasic.py
@@ -0,0 +1,32 @@
+#1.  data Type Automatically
+
+#1. int datatype
+print("\n############int datatype##################")
+a=10
+print(a)
+print(type(a))
+
+#2. float dataType
+print("\n###########float dataType####################")
+f=10.34
+print(f)
+print(type(f))
+
+#3. complex data Type
+print("\n############complex data Type################")
+com=40+4j
+print(com)
+print(type(com))
+
+
+#2. specific data type
+print("\n#############specific data type################")
+
+b=int(10)
+print(b)
+
+print("\n#############tyepcasting################")
+c=int("10") #tyepcasting
+print(c)
+print(type(c))
+
