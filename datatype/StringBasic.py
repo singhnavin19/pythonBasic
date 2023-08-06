43a55ca2b58dc8d9ee6c673291f3fb136f0d00b2
singhnavin19 <singhnavin19290@gmail.com>
2023-08-06 12:29:01 +0530

    string basic

---
+++ datatype/StringBasic.py
@@ -0,0 +1,18 @@
+#1. by uisng double quote
+firstName='navin,"fist name"'
+print(firstName)
+print(type(firstName))
+
+#2. by uisng single quote
+lastName="singh's lastname" ;
+print(lastName)
+
+#3. by using triple quote
+
+fullName='''No Internet
+Try:
+
+Checking the network cables, modem and router
+Reconnecting to Wi-Fi
+ERR_INTERNET_DISCONNECTED'''
+print(fullName)
