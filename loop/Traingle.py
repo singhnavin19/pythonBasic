542478ad6828b680b1536216aa6f4f4a2c455ea4
singhnavin19 <singhnavin19290@gmail.com>
2023-08-29 21:39:16 +0530

    fact code and cost

--- loop/Traingle.py
+++ loop/Traingle.py
@@ -58,3 +58,27 @@ for i in range(1,6):
     for i1 in range(0,i):
         print("*",end="")
     print()
+
+print("factorial code")
+no=int(input("please enter number"))
+factorial=1
+if no==0:
+    print(factorial)
+elif no<0:
+    print("negative -12number doesn't have factorial")
+else:    
+    for i in range(1,no+1):
+            factorial=factorial*i
+    print(factorial)
+
+print("buy code")
+
+buyCount=int(input("please enter count"))
+cost=1
+if buyCount<10:
+    cost=buyCount*120
+elif buyCount>10 and buyCount<=99:
+    cost=buyCount*100
+else:
+    cost=buyCount*70
+print(cost)
