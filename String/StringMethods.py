cf710fa59643ebbe9d4b9ae8621049da7297a588
singhnavin19 <singhnavin19290@gmail.com>
2023-09-24 11:46:21 +0530

    string methods

---
+++ String/StringMethods.py
@@ -0,0 +1,27 @@
+name="hello i am hiranmoy, I am learning python"
+
+name_partion=name.partition('am')
+print(name_partion)
+
+name_split=name.split()
+print(name_split)
+
+name_capitalize=name.capitalize()
+print(name_capitalize)
+
+name_title=name.title()
+print(name_title)
+
+name_upper=name.upper()
+print(name_upper)
+
+name_lower=name.lower()
+print(name_lower)
+
+name_join='*'.join(name)
+print(name_join)
+
+name_empty='*'.join('')
+print(name_empty)
+
+
