c05b6f2c766e6bbd8797dd0539dc11f118cc5300
singhnavin19 <singhnavin19290@gmail.com>
2026-01-11 13:58:06 +0530

    operator example

---
+++ Operators/ArithmaticExample.py
@@ -0,0 +1,18 @@
+no1=int(input("no1 daliye sir ji"))
+no2=int(input("no2 bhi daal deijiye please"))
+print(no1,no2)
+
+print("no+no2=",no1+no2)
+print("no-no2=",no1-no2)
+print("no*no2=",no1*no2)
+print("no/no2=",no1/no2)
+print("no//no2=",no1//no2)
+print("no%no2=",no1%no2)
+
+no3=no1+no2
+print(no3)
+no1=no1+10
+
+
+
+
---
+++ Operators/AssignMentOps.py
@@ -0,0 +1,10 @@
+bill=10000
+print("bill=",bill)
+
+bill-=2000 # bill-=2000
+bill+=2000 # bill-=2000
+bill*=bill
+bill/=bill
+print("final bill=",bill)
+
+
