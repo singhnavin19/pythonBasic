92c75f15f73cbc5299b2e2bb0f570690c8f708c4
singhnavin19 <singhnavin19290@gmail.com>
2026-01-15 19:34:07 +0530

    conditional statement

---
+++ Operators/ComparisionOps.py
@@ -0,0 +1,3 @@
+marks10th=77
+marks12th=55
+print(marks10th>marks12th)
---
+++ conditional/IfExample.py
@@ -0,0 +1,9 @@
+age=int(input("please enter your age"))
+print("your age is ",age)
+if age > 18:
+    print("Eligible to apply voting card")
+    print("Eligible to apply Pancard")
+else:
+    print("Not aligible to apply voting card")
+    print("abhi maze karo ")
+    print()
